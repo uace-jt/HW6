bf73114c5e1015d0e81b61f548469254f10979c3
uace-jt <wilson950403@gmail.com>
2024-12-01 20:14:28 +0800

    Add files via upload

---
+++ P10-7.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+struct card {
+    const char* face;
+    const char* suit;
+};
+
+int main()
+{
+    struct card aCard;
+    struct card* cardPtr;
+
+    aCard.face = "Ace";
+    aCard.suit = "Spades";
+
+    cardPtr = &aCard;
+
+    
+    printf("%s of %s\n", aCard.face, aCard.suit);       
+    printf("%s of %s\n", cardPtr->face, cardPtr->suit); 
+    printf("%s of %s\n", (*cardPtr).face, (*cardPtr).suit);
+
+    system("pause");
+    return 0;
+}
